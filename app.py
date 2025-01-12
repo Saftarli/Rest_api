89b8b8cd9700c574d37b933f6bda8816c249939b
saftarli <tofiqsefterli@gmail.com>
2025-01-12 23:03:40 +0400

    2 ci video

--- app.py
+++ app.py
@@ -5,25 +5,43 @@ client = app.test_client()
 
 tutorials = [
     {
+        'id': 1,
         'tittle': 'Video1 ',
         'description': 'Get, post routes'
     },
     {
+        'id': 2,
         'tiitle': 'Video2',
         'description': 'Put, Delete  routes'
     }
 ]
 
-@app.route('/tutorials', methods = ['GET'])
+@app.route('/tutorials', methods=['GET'])
 def get_list():
     return jsonify(tutorials)
 
-@app.route('/tutorials', methods = ['POST'])
+@app.route('/tutorials', methods=['POST'])
 def update_list():
     new_one = request.json
     tutorials.append(new_one)
-    return jsonify(tutorials)
+    return jsonify(tutorials), 201
 
+@app.route('/tutorials/<int:tutorial_id>', methods=['PUT'])
+def update_tutorial(tutorial_id):
+    item = next((x for x in tutorials if x['id'] == tutorial_id), None)
+    if not item:
+        return {'message': 'No tutorials with this ID'}, 404
+    params = request.json
+    item.update(params)
+    return jsonify(item)
 
-if __name__ == '__name__':
+@app.route('/tutorials/<int:tutorial_id>', methods=['DELETE'])
+def delete_tutorial(tutorial_id):
+    idx, item = next((i, x) for i, x in enumerate(tutorials) if x[1]['id'] == tutorial_id), (None, None)
+    if idx is None:
+        return {'message': 'No tutorials with this ID'}, 404
+    tutorials.pop(idx)
+    return '', 204
+
+if __name__ == '__main__':
     app.run()
